99fd58cf5aaf6c9ee27361b2915b89a2ad291707
tomludden <148908140+tomludden@users.noreply.github.com>
2025-09-27 23:50:24 +0200

    v

--- index.js
+++ index.js
@@ -13,9 +13,18 @@ const app = express()
 connectDB()
 
 app.use(express.json())
-
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
-
+/* 
+app.use(
+  cors({
+    origin:
+      'https://starlit-semolina-e24ea6.netlify.app/' || 'http://localhost:3000' 
+  })
+) */
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || '*'
+  })
+)
 app.use('/api/v1/products', productsRouter)
 app.use('/api/v1/users', usersRouter)
 
