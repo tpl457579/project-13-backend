ddd55a9507c3d6a63720f617b716fde351383fe3
thomas-ludden <tpl457579@gmail.com>
2026-02-12 05:14:13 +0000

    m mkjkh

--- src/utils/scraper.js
+++ src/utils/scraper.js
@@ -1,7 +1,7 @@
 import puppeteer from 'puppeteer';
 import mongoose from 'mongoose';
 import { globSync } from 'glob';
-import Product from '../api/products.js'; 
+import Product from '/xapi/products.js'; 
 import 'dotenv/config';
 
 export const scrapeProducts = async () => {
