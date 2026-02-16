e5333cd1f1addbb4450dd4d5cc3450874bf0a68c
thomas-ludden <tpl457579@gmail.com>
2026-02-16 22:21:52 +0100

    c

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
       family: 4,
       serverSelectionTimeoutMS: 5000
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${connection.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
